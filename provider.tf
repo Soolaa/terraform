13e57eac9904e06d0f6206c807f4d8625836559d
soolaa <sahar.nab67@gmail.com>
2024-04-08 07:59:23 +0330

    useing variable

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-
 }
 
 # terraform {
@@ -18,8 +17,6 @@ terraform {
     bucket     = "sahar-state-file"
     key        = "state-file"
     region     = "us-east-1"
-    access_key = "AKIAWEGVD6X3ZW6Y3KE3"
-    secret_key = "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"
   }
   required_providers {
     aws = {
